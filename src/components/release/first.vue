b939f56c55cd6d32947303b17cbfbc4706945583
liaowentuan <996002654@qq.com>
2018-03-01 17:28:55 +0800

    change

--- src/components/release/first.vue
+++ src/components/release/first.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
-    {{msg}}
-    <br>
-    {{count}}
+    <el-row class="search">
+      <el-col :span="5">
+        <el-input v-model="name"></el-input>
+      </el-col>
+      <el-col :span="5" :offset="1">
+        <el-button class="el-button--primary" @click="increment">立即查询</el-button>
+        <el-button class="el-button--primary" @click="decrement">--</el-button>
+        <p>{{count}}</p>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -11,12 +18,19 @@ export default {
   name: 'first',
   data () {
     return {
-      msg: 'this is first'
+      msg: 'this is first',
+      name: ''
     }
   },
   methods: {
     cons (item) {
       console.log('这是底下的方法', item)
+    },
+    increment () {
+      this.$store.commit('increment')
+    },
+    decrement () {
+      this.$store.commit('decrement')
     }
   },
   computed: {
@@ -24,6 +38,9 @@ export default {
       this.cons(this.$store.state.count)
       return this.$store.state.count
     }
+  },
+  destroyed () {
+    this.$store.commit('setcrement')
   }
 }
 </script>
--- src/components/release/release.vue
+++ src/components/release/release.vue
@@ -1,26 +1,11 @@
 <template>
   <div class="release">
     <div class="menu">
-      <!--<ul>
-        <li><router-link to="realTime"><el-button>请假管理</el-button></router-link></li>
-        <li><router-link to="realTime"><el-button>放行管理</el-button></router-link></li>
-        <li><router-link to="realTime"><el-button>请假管理</el-button></router-link></li>
-      </ul>-->
       <el-radio-group v-model="item" style="margin-bottom: 30px;" @change="tabClick(item)">
         <el-radio-button label="first">请假管理</el-radio-button>
         <el-radio-button label="second">放行管理</el-radio-button>
         <el-radio-button label="third">请假管理</el-radio-button>
       </el-radio-group>
-      <el-row class="search">
-        <el-col :span="5">
-          <el-input v-model="name"></el-input>
-        </el-col>
-        <el-col :span="5" :offset="1">
-          <el-button class="el-button--primary" @click="increment">立即查询</el-button>
-          <el-button class="el-button--primary" @click="decrement">--</el-button>
-          <p>{{count}}</p>
-        </el-col>
-      </el-row>
     </div>
     <div>
       <router-view/>
@@ -41,21 +26,7 @@ export default {
   methods: {
     tabClick (item) {
       this.$router.replace({path: `/home/release/${item}`})
-    },
-    increment () {
-      this.$store.commit('increment')
-    },
-    decrement () {
-      this.$store.commit('decrement')
     }
-  },
-  computed: {
-    count () {
-      return this.$store.state.count
-    }
-  },
-  destroyed () {
-    this.$store.commit('setcrement')
   }
 }
 </script>
@@ -63,7 +34,6 @@ export default {
 <style scoped lang="less">
   .release{
     .menu{
-      border-bottom: 5px solid #f2f2f2;
       padding:10px 20px;
     }
     .el-radio-button:focus{
