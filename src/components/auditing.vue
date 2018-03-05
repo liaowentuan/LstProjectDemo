912a5ec3f58326a5a8f7bd922a982dfc50764df6
liaowentuan <996002654@qq.com>
2018-03-05 14:31:33 +0800

    echarts

--- src/components/auditing.vue
+++ src/components/auditing.vue
@@ -1,18 +1,81 @@
 <template>
-  <div>{{msg}}</div>
+  <div :class="className" :id="id" :style="{height:height,width:width}" ref="myEchart">
+  </div>
 </template>
-
 <script>
 export default {
-  name: 'auditing',
+  props: {
+    className: {
+      type: String,
+      default: 'yourClassName'
+    },
+    id: {
+      type: String,
+      default: 'yourID'
+    },
+    width: {
+      type: String,
+      default: '500px'
+    },
+    height: {
+      type: String,
+      default: '500px'
+    }
+  },
   data () {
     return {
-      msg: 'this is auditing'
+      chart: null
+    }
+  },
+  mounted () {
+    this.$nextTick(function () {
+      this.initChart()
+    })
+  },
+  beforeDestroy () {
+    if (!this.chart) {
+      return
+    }
+    this.chart.dispose()
+    this.chart = null
+  },
+  methods: {
+    initChart () {
+      // console.log(this.$refs.myEchart) 获取元素
+      this.chart = this.echarts.init(this.$refs.myEchart)
+      // 把配置和数据放这里
+      this.chart.setOption({
+        color: ['#3398DB'],
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: { // 坐标轴指示器，坐标轴触发有效
+            type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [{
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+          axisTick: {
+            alignWithLabel: true
+          }
+        }],
+        yAxis: [{
+          type: 'value'
+        }],
+        series: [{
+          name: '直接访问',
+          type: 'bar',
+          barWidth: '60%',
+          data: [10, 52, 200, 334, 390, 330, 220]
+        }]
+      })
     }
   }
 }
 </script>
-
-<style scoped>
-
-</style>
--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import 'element-ui/lib/theme-chalk/index.css'
 import axios from 'axios'
 import App from './App'
 import store from './store/store'
+import echarts from 'echarts'
 // 添加请求拦截器
 axios.interceptors.request.use(function (config) {
   // 在发送请求之前做些什么
@@ -24,6 +25,7 @@ axios.interceptors.response.use(function (response) {
   // 对响应错误做点什么
   return Promise.reject(error)
 })
+Vue.prototype.echarts = echarts
 Vue.prototype.$http = axios
 Vue.use(ElementUI)
 
@@ -39,6 +41,7 @@ new Vue({
   el: '#app',
   router,
   store,
+  echarts,
   components: { App },
   template: '<App/>'
 })
